d09451f17b8aea95643e047bb4c34b02dce6f5b5
Ricardo Aguilar <jraguilars@icloud.com>
2021-03-29 00:07:59 -0600

    card details image effect

--- src/Components/CardDetails.jsx
+++ src/Components/CardDetails.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+
+
 const CardDetails = ({data}) => {
-    const {id: charId, name, type, species, image, location: {name: locName} } = data;
+    const {id: charId, name, type, status, species, image, location: {name: locName} } = data;
+    const getStatus = (status) => {
+        let color = "text-green-400";
+        if(status === "Dead") {
+          color = "text-red-700";
+        }
+        if(status === "unknown"){
+          color = "text-gray-400";
+        }
+        return color;
+      };
     return (
-    <div className="mx-4 w-full flex flex-row border-gray-100 border-solid">
-        <img className="w-1/3 rounded-lg" src={image} alt="Character image"/>
-        <div className="w-full text-left p-4 text-lg text-white">
+    <div className="mx-4 w-full flex flex-row content-around border-gray-100 border-solid">
+        <div className="image-container rounded-lg mr-4">
+            <img className="image w-auto h-auto rounded-lg" src={image} alt="Character image"/>
+        </div>
+        <div className="w-full text-left p-4 text-lg text-white grid grid-cols-auto content-between">
             <p>{`Alias: ${name}`}</p>
+            <p>Status: <span className={getStatus(status)}>{status}</span></p>
             <p>{`Last location known: ${locName}`}</p>
         </div>
     </div>
